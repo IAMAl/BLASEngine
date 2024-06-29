c5b1bedad2626d126cf8526f81181fd619d4fe41
takano <rentsrule@gmail.com>
2024-06-29 10:24:21 +0900

    add: reorder buff for vector unit

---
+++ src_hdl/TPU/backend/scalarunit/reorderbuff_v.sv
@@ -0,0 +1,80 @@
+module reorderbuff_v #(
+	parameter NUM_ENTRY = 16;
+)(
+	input						clock,
+	input						reset,
+	input						I_Store,
+	input	issue_no_t			I_Issue_No,
+	input						I_Commmit_Req,
+	input	issue_no_t			I_Commit_No,
+	output	logic				O_Commit_Req,
+	output	issue_no_t			O_Commit_No,
+	output	logic				O_Full
+);
+
+
+	localparam WIDTH_ENTRY		= $clog2(NUM_ENTYRY);
+
+	commit_tab_v				Commit_V	[NUM_ENTRY-1:0];
+
+	logic	[NUM_ENTRY-1:0]		Clr_Valid;
+	logic	[NUM_ENTRY-1:0]		Set_Commit;
+
+	logic						We;
+	logic						Re;
+	logic	[WIDTH_ENTRY-1:0]	WNo;
+	logic	[WIDTH_ENTRY-1:0]	RNo;
+	logic						Empty;
+	logic						Full;
+
+	assign O_Commit_Req			= Re;
+	assign O_Commit_No			= Commit_V[RNo].Issue_No;
+	assign O_Full				= Full;
+
+	assign Re					= Commit_V[RNo].Valid & Commit_V[RNo].Commit;
+	assign We					= I_Store & ~Full;
+
+	always_comb: begin
+		for ( int i=0; i<NUM_ENTRY; ++i ) begin
+			Set_Commit[i]		= Commit_V[i].En_Lane ^ Commit_V[i].Commit;
+		end
+	end
+
+	always_ff @( posedge clock ) begin
+		if ( reset ) begin
+			for ( int i=0; i<NUM_ENTRY; ++i ) begin
+				Commit_V[i]		<= '0;
+			end
+		end
+		else if ( I_Store | I_Commmit_Req ) begin
+			if ( I_Store ) begin
+				Commit_V[i].Valid	<= 1'b1;
+				Commit_V[i].Issue_No<= I_Issue_No;
+				Commit_V[i].EN_Lane	<= I_En_Lane;
+				Commit_V[i].Commit	<= 0;
+			end
+
+			if ( I_Commmit_Req ) begin
+				Commit_V[i].Valid	<= 1'b0;
+			end
+		end
+	end
+
+
+	//// Module: Ring-Buffer Controller
+	RingBuffCTRL #(
+		.NUM_ENTRY(			NUM_ENTRY					)
+	) RingBuffCTRL
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_We(				We							),
+		.I_Re(				Re							),
+		.O_WAddr(			WNo							),
+		.O_RAddr(			RNo							),
+		.O_Full(			Full						),
+		.O_Empty(			Empty						),
+		.O_Num(											)
+	);
+
+endmodule
