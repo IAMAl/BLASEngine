a52b64c4c47dd8eee69cf520a9a46de00eca0d15
takano <rentsrule@gmail.com>
2024-06-29 15:36:59 +0900

    add: buffer for routing data in network unit

--- src_hdl/TPU/backend/common/network.sv
+++ src_hdl/TPU/backend/common/network.sv
@@ -4,19 +4,19 @@ module Network
 	parameter int NUM_LANES		= 16,
 	parameter int WIDTH_LANES	= $clog2(NUM_LANES);
 )(
-	input	[NUM_LANES+2-1:0]	I_Config_Path,					//Path Selects
-	input	[2:0]				I_Bypass_Path,					//Path Selects
-	input	[2:0]				I_WB_Path1,						//Path Selects
-	input	[2:0]				I_WB_Path2,						//Path Selects
-	input	hop_data_t			I_Path_Hop,						//From Multi-Hop Length Path
+	input	[8:0]				I_Config_Path,					//Path Selects
+	input	issue_no_t			I_Issue_No,						//Issue No from Reg Read Pipe
+	input	issue_no_t			I_Issue_No_WB1,					//Commit No from Write-Back-1
+	input	issue_no_t			I_Issue_No_WB2,					//Commit No from Write-Back-2
+	input	data_t				I_Path_Data,					//From Hop Length Path
 	input	data_t				I_Scalar_Data,					//From Scalar Unit
+	input	data_t				I_Src_Data1,					//From RegFile after Rotation Path
+	input	data_t				I_Src_Data2,					//From RegFile after Rotation Path
+	input	data_t				I_Src_Data3,					//From RegFile after Rotation Path
 	input	index_t				I_WB_Index1,					//From ALU
 	input	index_t				I_WB_Index2,					//From ALU
 	input	data_t				I_WB_Data1,						//From ALU
 	input	data_t				I_WB_Data2,						//From ALU
-	input	data_t				I_Src_Data1,					//From RegFile after Rotation Path
-	input	data_t				I_Src_Data2,					//From RegFile after Rotation Path
-	input	data_t				I_Src_Data3,					//From RegFile after Rotation Path
 	output	data_t				O_Src_Data1,					//To Exec Unit
 	output	data_t				O_Src_Data2,					//To Exec Unit
 	output	data_t				O_Src_Data3,					//To Exec Unit
@@ -31,15 +31,20 @@ module Network
 );
 
 
-	logic	[WIDTH_LANES-1:0]	Sel_Path;
+	logic						Sel_Data1;
 	logic						Sel_Data2;
 	logic						Sel_Data3;
-	logic						Sel_Bypass1;
-	logic						Sel_Bypass2;
-	logic						Sel_Bypass3;
+
+	logic						Sel_Bypass11;
+	logic						Sel_Bypass12;
+	logic						Sel_Bypass13;
+
+	logic						Sel_Bypass21;
+	logic						Sel_Bypass22;
+	logic						Sel_Bypass23;
+
 	logic						Sel_Scalar1;
 	logic						Sel_Scalar2;
-	logic						Sel_Scalar3;
 	data_t						Path_Data;
 
 	logic						Sel_Addr_Data1;
@@ -67,12 +72,22 @@ module Network
 	logic						Sel_Path_Even1;
 	logic						Sel_Path_Even2;
 
+	assign O_Src_Data1			= ( Bypass_Data1 ) ? I_WB_Data1 : Src_Data1;
+	assign O_Src_Data2			= ( Bypass_Data2 ) ? I_WB_Data2 : Src_Data2;
+	assign O_Src_Data3			= ( Bypass_Data3 ) ? I_WB_Data1 : Src_Data3;
 
-	assign Sel_Data2			= I_Config_Path[0];
-	assign Sel_Data3			= I_Config_Path[1];
-	assign Sel_Path				= I_Config_Path[WIDTH_LANES+2-1:2];
 
+	// Source Operands
+	assign Sel_Data1			= I_Config_Path[0];
+	assign Sel_Data2			= I_Config_Path[1];
+	assign Sel_Data3			= I_Config_Path[2];
 
+	// Scalar Data
+	assign Sel_Scalar1			= I_Config_Path[4:3] == 0;
+	assign Sel_Scalar2			= I_Config_Path[4:3] == 1;
+	assign Sel_Scalar3			= I_Config_Path[4:3] == 2;
+
+	// Bypass
 	assign Sel_Bypass11			= I_Bypass_Path == 4'h0;
 	assign Sel_Bypass12			= I_Bypass_Path == 4'h1;
 	assign Sel_Bypass13			= I_Bypass_Path == 4'h2;
@@ -81,106 +96,345 @@ module Network
 	assign Sel_Bypass22			= I_Bypass_Path == 4'h5;
 	assign Sel_Bypass23			= I_Bypass_Path == 4'h6;
 
+	// Write-Back
+	assign Sel_WB_Scalar1		= I_Config_Path[6:5] == 3'h0;
+	assign Sel_WB_Path1			= I_Config_Path[6:5] == 3'h1;
+	assign Sel_WB_Bypass11		= I_Config_Path[6:5] == 3'h2;
+	assign Sel_WB_Bypass12		= I_Config_Path[6:5] == 3'h3;
 
-	assign Sel_Addr_Data1		= ( I_Bypass_Path == 4'h8 ) | ( I_Bypass_Path == 4'hc );
-	assign Sel_Addr_Data2		= ( I_Bypass_Path == 4'h9 ) | ( I_Bypass_Path == 4'hd );
-	assign Sel_Addr_Data3		= ( I_Bypass_Path == 4'ha ) | ( I_Bypass_Path == 4'he );
+	assign Sel_WB_Scalar2		= I_Config_Path[8:7] == 3'h0;
+	assign Sel_WB_Path2			= I_Config_Path[8:7] == 3'h1;
+	assign Sel_WB_Bypass21		= I_Config_Path[8:7] == 3'h2;
+	assign Sel_WB_Bypass22		= I_Config_Path[8:7] == 3'h3;
 
-	assign Sel_Stride_Data1		= ( I_Bypass_Path == 4'hb ) | ( I_Bypass_Path == 4'he );
-	assign Sel_Stride_Data2		= ( I_Bypass_Path == 4'h8 ) | ( I_Bypass_Path == 4'hf );
-	assign Sel_Stride_Data3		= ( I_Bypass_Path == 4'h9 ) | ( I_Bypass_Path == 4'hc );
+	// Source Operands
+	assign Src_Data			= {
+									I_Scalar_Data,
+									I_Src_Data2,
+									I_Src_Data3,
+									I_WB_Data1,
+									I_WB_Data2,
+									I_Scalar_Data,
+									I_Path_Data
+								};
 
-	assign Sel_Length_Data1		= ( I_Bypass_Path == 4'ha ) | ( I_Bypass_Path == 4'hf );
-	assign Sel_Length_Data2		= ( I_Bypass_Path == 4'hb ) | ( I_Bypass_Path == 4'he );
-	assign Sel_Length_Data3		= ( I_Bypass_Path == 4'h8 ) | ( I_Bypass_Path == 4'hd );
+	always_comb: begin
+		if ( Sel_Data1 )			assign Sel_Src_Data1= 0;
+		else if ( Sel_Bypass13 )	assign Sel_Src_Data1= 3;
+		else if ( Sel_Bypass23 )	assign Sel_Src_Data1= 4;
+		else if ( Sel_Scalar1 )		assign Sel_Src_Data1= 5;
+		else 						assign Sel_Src_Data1= 6;
+	end
+
+	always_comb: begin
+		if ( Sel_Data2 )			assign Sel_Src_Data2= 1;
+		else if ( Sel_Bypass12 )	assign Sel_Src_Data2= 3;
+		else if ( Sel_Bypass22 )	assign Sel_Src_Data2= 4;
+		else if ( Sel_Scalar2 )		assign Sel_Src_Data2= 5;
+		else 						assign Sel_Src_Data2= 6;
+	end
+
+	always_comb: begin
+		if ( Sel_Data3 )			assign Sel_Src_Data3= 2;
+		else if ( Sel_Bypass13 )	assign Sel_Src_Data3= 3;
+		else if ( Sel_Bypass23 )	assign Sel_Src_Data3= 4;
+		else if ( Sel_Scalar3 )		assign Sel_Src_Data3= 5;
+		else 						assign Sel_Src_Data3= 6;
+	end
+
+	// Write-Back
+	assign WB_Data_Src			= {
+									I_Scalar_Data,
+									I_Path_Data,
+									I_WB_Data1,
+									I_WB_Data2
+								};
+
+	assign WB_Index_Src			= {
+									I_Scalar_Index,
+									I_Path_Index,
+									I_WB_Index1,
+									I_WB_Index2
+								};
+
+	always_comb: begin
+		if ( Sel_WB_Scalar1 )		assign Sel_WB_Data1	= 0;
+		else if ( Sel_WB_Path1 )	assign Sel_WB_Data1	= 1;
+		else if ( Sel_WB_Bypass21 )	assign Sel_WB_Data1	= 2;
+		else if ( Sel_WB_Bypass22 )	assign Sel_WB_Data1	= 3;
+		else 						assign Sel_WB_Data1	= 0;
+	end
+
+	always_comb: begin
+		if ( Sel_WB_Scalar2 )		assign Sel_WB_Data2	= 0;
+		else if ( Sel_WB_Path2 )	assign Sel_WB_Data2	= 1;
+		else if ( Sel_WB_Bypass21 )	assign Sel_WB_Data2	= 2;
+		else if ( Sel_WB_Bypass22 )	assign Sel_WB_Data2	= 3;
+		else 						assign Sel_WB_Data2	= 0;
+	end
+
+	// Load/Store Unit
+	assign Data_Src_LdSt		= {
+									I_Src_Data1,
+									I_Src_Data2,
+									I_Src_Data3
+								};
+
+	always_comb: begin
+		case ( I_Bypass_Path )
+		4'h8:	assign Sel_Addr_Data	= 0;
+		4'h9:	assign Sel_Addr_Data	= 1;
+		4'ha:	assign Sel_Addr_Data	= 2;
+		4'hc:	assign Sel_Addr_Data	= 0;
+		4'hd:	assign Sel_Addr_Data	= 1;
+		4'he:	assign Sel_Addr_Data	= 2;
+		default:assign Sel_Addr_Data	= 0;
+		endcase
+	end
+
+	always_comb: begin
+		case ( I_Bypass_Path )
+		4'h8:	assign Sel_Stride_Data	= 1;
+		4'h9:	assign Sel_Stride_Data	= 2;
+		4'hb:	assign Sel_Stride_Data	= 0;
+		4'hc:	assign Sel_Stride_Data	= 2;
+		4'he:	assign Sel_Stride_Data	= 0;
+		4'hf:	assign Sel_Stride_Data	= 1;
+		default:assign Sel_Stride_Data	= 0;
+		endcase
+	end
+
+	always_comb: begin
+		case ( I_Bypass_Path )
+		4'h8:	assign Sel_Length_Data	= 2;
+		4'ha:	assign Sel_Length_Data	= 0;
+		4'hb:	assign Sel_Length_Data	= 1;
+		4'hd:	assign Sel_Length_Data	= 2;
+		4'he:	assign Sel_Length_Data	= 1;
+		4'hf:	assign Sel_Length_Data	= 0;
+		default:assign Sel_Length_Data	= 0;
+		endcase
+	end
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		3							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_Src_Data1
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			1							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_Src_Data1				),
+		.I_Issue_No(		I_Issue_No					),
+		.I_Issue_No_WB(		I_Issue_No_WB1				),
+		.I_Data(			Src_Data					),
+		.O_Data(			Src_Data1					),
+		.O_Bypass(			Bypass_Data1				),
+		.O_Re(				Re_Src_Data1				)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		3							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_Src_Data2
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			1							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_Src_Data2				),
+		.I_Issue_No(		I_Issue_No					),
+		.I_Issue_No_WB(		I_Issue_No_WB2				),
+		.I_Data(			Src_Data					),
+		.O_Data(			Src_Data2					),
+		.O_Bypass(			Bypass_Data2				),
+		.O_Re(				Re_Src_Data2				)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		3							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_Src_Data3
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			1							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_Src_Data3				),
+		.I_Issue_No(		I_Issue_No					),
+		.I_Issue_No_WB(		I_Issue_No_WB1				),
+		.I_Data(			WB_Index_Src				),
+		.O_Data(			Src_Data3					),
+		.O_Bypass(			Bypass_Data3				),
+		.O_Re(				Re_Src_Data3				)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		2							),
+		.TYPE_DEF(			index_t						)
+	) Path_Sel_WB_Index1
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			0							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_WB_Data1				),
+		.I_Issue_No(		0							),
+		.I_Issue_No_WB(		0							),
+		.I_Data(			WB_Index_Src				),
+		.O_Data(			O_WB_Index1					),
+		.O_Bypass(										),
+		.O_Re(											)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		2							),
+		.TYPE_DEF(			index_t						)
+	) Path_Sel_WB_Index2
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			0							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_WB_Data2				),
+		.I_Issue_No(		0							),
+		.I_Issue_No_WB(		0							),
+		.I_Data(			WB_Data_Src					),
+		.O_Data(			O_WB_Index2					),
+		.O_Bypass(										),
+		.O_Re(											)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		2							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_WB_Data1
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			0							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_WB_Data1				),
+		.I_Issue_No(		0							),
+		.I_Issue_No_WB(		0							),
+		.I_Data(			WB_Data_Src					),
+		.O_Data(			O_WB_Data1					),
+		.O_Bypass(										),
+		.O_Re(											)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		2							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_WB_Data2
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			0							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_WB_Data2				),
+		.I_Issue_No(		0							),
+		.I_Issue_No_WB(		0							),
+		.I_Data(			WB_Data_Src					),
+		.O_Data(			O_WB_Data2					),
+		.O_Bypass(										),
+		.O_Re(											)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		2							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_Address
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			0							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_Addr_Data				),
+		.I_Issue_No(		0							),
+		.I_Issue_No_WB(		0							),
+		.I_Data(			Data_Src_LdSt				),
+		.O_Data(			O_Address					),
+		.O_Bypass(										),
+		.O_Re(											)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		2							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_Stride
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_En_WB(			0							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_Stride_Data				),
+		.I_Issue_No(		0							),
+		.I_Issue_No_WB(		0							),
+		.I_Data(			Data_Src_LdSt				),
+		.O_Data(			O_Stride					),
+		.O_Bypass(										),
+		.O_Re(											)
+	);
+
+	path_sel #(
+		.NUM_ENTRY(			NUM_ENTRY_ROUTE				),
+		.WIDTH_NUM_SRC(		2							),
+		.TYPE_DEF(			data_t						)
+	) Path_Sel_Length
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_WNo(				WNo							),
+		.I_RNo(				RNo							),
+		.I_En_WB(			0							),
+		.I_Stall(			I_Stall						),
+		.I_Store(			I_Req						),
+		.I_Sel(				Sel_Length_Data				),
+		.I_Issue_No(		0							),
+		.I_Issue_No_WB(		0							),
+		.I_Data(			Data_Src_LdSt				),
+		.O_Data(			O_Length					),
+		.O_Bypass(										),
+		.O_Re(											)
+		);
 
 
-	assign Sel_WB_Data11		= I_WB_Path1 == 3'h1;
-	assign Sel_WB_Data12		= I_WB_Path1 == 3'h2;
-	assign Sel_WB_Data13		= I_WB_Path1 == 3'h3;
-	assign Sel_WB_Bypass11		= I_WB_Path1 == 3'h4;
-	assign Sel_WB_Bypass12		= I_WB_Path1 == 3'h5;
-	assign Sel_Path_Odd1		= I_WB_Path1 == 3'h6;
-	assign Sel_Path_Even1		= I_WB_Path1 == 3'h7;
-
-	assign Sel_WB_Data21		= I_WB_Path2 == 3'h1;
-	assign Sel_WB_Data22		= I_WB_Path2 == 3'h2;
-	assign Sel_WB_Data23		= I_WB_Path2 == 3'h3;
-	assign Sel_WB_Bypass21		= I_WB_Path2 == 3'h4;
-	assign Sel_WB_Bypass22		= I_WB_Path2 == 3'h5;
-	assign Sel_Path_Odd2		= I_WB_Path2 == 3'h6;
-	assign Sel_Path_Even2		= I_WB_Path2 == 3'h7;
-
-
-	//ToDo
-	assign Sel_WB_Index1_Idx1	=;
-	assign Sel_WB_Index1_Idx2	=;
-	assign Sel_WB_Index2_Idx1	=;
-	assign Sel_WB_Index2_Idx2	=;
-
-
-	assign Path_Data			=I_Path_Hop[ Sel_Path ];
-
-
-	assign O_Src_Data1			= ( Sel_Bypass11 ) ?		I_WB_Data1 :
-									( Sel_Bypass21 ) ?		I_WB_Data2 :
-									( Sel_Scalar1 ) ?		I_Scalar_Data :
-															I_Src_Data1;
-
-	assign O_Src_Data2			= ( Sel_Data2 ) ? 			I_Src_Data2 :
-									( Sel_Bypass12 ) ?		I_WB_Data1 :
-									( Sel_Bypass22 ) ?		I_WB_Data2 :
-									( Sel_Scalar2 ) ?		I_Scalar_Data :
-															Path_Data;
-
-	assign O_Src_Data3			= ( Sel_Data3 ) ? 			I_Src_Data3 :
-									( Sel_Bypass13 ) ?		I_WB_Data1 :
-									( Sel_Bypass23 ) ?		I_WB_Data2 :
-									( Sel_Scalar3 ) ?		I_Scalar_Data :
-															Path_Data;
-
-
-	assign O_WB_Index1			= ( Sel_WB_Index1_Idx1 ) ?	I_WB_Index1 :
-									( Sel_WB_Index1_Idx2 ) ?I_WB_Index2 :
-															0;
-
-	assign O_WB_Index2			= ( Sel_WB_Index2_Idx2 ) ?	I_WB_Index2 :
-									( Sel_WB_Index2_Idx1 ) ?I_WB_Index1 :
-															0;
-
-
-	assign O_WB_Data1			= ( Sel_WB_Data11 ) ?		I_Src_Data1 :
-									( Sel_WB_Data12 ) ?		I_Src_Data2 :
-									( Sel_WB_Data13 ) ?		I_Src_Data3 :
-									( Sel_WB_Bypass11 ) ?	I_WB_Data1 :
-									( Sel_WB_Bypass12 ) ?	I_WB_Data2 :
-									( Sel_Path_Odd1 ) ?		I_Odd_Path :
-									( Sel_Path_Even1 ) ?	I_Even_Path :
-															0;
-
-	assign O_WB_Data2			= ( Sel_WB_Data23 ) ?		I_Src_Data3 :
-									( Sel_WB_Data22 ) ?		I_Src_Data2 :
-									( Sel_WB_Data21 ) ?		I_Src_Data1 :
-									( Sel_WB_Bypass21 ) ?	I_WB_Data1 :
-									( Sel_WB_Bypass22 ) ?	I_WB_Data2 :
-									( Sel_Path_Odd2 ) ?		I_Odd_Path :
-									( Sel_Path_Even2 ) ?	I_Even_Path :
-															0;
-
-
-	assign O_Address			= ( Sel_Addr_Data1 ) ?		I_Src_Data1 :
-									( Sel_Addr_Data2 ) ?	I_Src_Data2 :
-									( Sel_Addr_Data3 ) ?	I_Src_Data3 :
-															0;
-
-	assign O_Stride				= ( Sel_Stride_Data1 ) ?	I_Src_Data1 :
-									( Sel_Stride_Data2 ) ?	I_Src_Data2 :
-									( Sel_Stride_Data3 ) ?	I_Src_Data3 :
-															0;
-
-	assign O_Length				= ( Sel_Length_Data1 ) ?	I_Src_Data1 :
-									( Sel_Length_Data2 ) ?	I_Src_Data2 :
-									( Sel_Length_Data3 ) ?	I_Src_Data3 :
-															0;
+		//// Module: Ring-Buffer Controller
+		assign We					= I_Store & ~Full;
+		assign Re					= ;//ToDo
+		RingBuffCTRL #(
+		.NUM_ENTRY(			NUM_ENTRY					)
+	) RingBuffCTRL
+	(
+		.clock(				clock						),
+		.reset(				reset						),
+		.I_We(				We							),
+		.I_Re(				Re							),
+		.O_WAddr(			WNo							),
+		.O_RAddr(			RNo							),
+		.O_Full(			Full						),
+		.O_Empty(			Empty						),
+		.O_Num(											)
+	);
 
 endmodule
--- src_hdl/TPU/backend/common/path_sel.sv
+++ src_hdl/TPU/backend/common/path_sel.sv
@@ -1,8 +1,12 @@
 module path_sel #(
-	parameter NUM_ENTRY			= 16
+	parameter NUM_ENTRY			= 16,
+	parameter WIDTH_NUM_SRC		= 4
 )(
 	input						clock,
 	input						reset,
+	input	[WIDTH_ENTRY-1:0]	I_WNo;					//Write Pointer
+	input	[WIDTH_ENTRY-1:0]	I_RNo;					//Read Pointer
+	input						I_En_WB,				//Enable Write-Back
 	input						I_Stall,				//Stall Request
 	input						I_Store,				//Store Request for Routing Data
 	input	[WIDTH_NUM_SRC-1:0]	I_Sel,					//Routing Data
@@ -19,46 +23,39 @@ module path_sel #(
 
 	logic						We;
 	logic						Re;
-	logic	[WIDTH_ENTRY-1:0]	WNo;
 	logic	[WIDTH_ENTRY-1:0]	RNo;
 	logic						Empty;
 	logic						Full;
 
 	logic	[WIDTH_NUM_SRC-1:0]	Sel_Src;
 
-
 	logic	[WIDTH_NUM_SRC-1:0]	SelBuff		[NUM_ENTRY-1:0];
 	issue_no_t					IssueNoBuff	[NUM_ENTRY-1:0];
-
 
 	logic						is_Match;
 
 
-	assign We					= I_Store & ~Full;
-	assign Re					= ( ~I_Stall | is_Match ) & ~Empty;
+	assign O_Re					= ( ~I_Stall | is_Match ) & ~Empty;
 
-	assign is_Match				= IssueNoBuff[RNo] == I_Issue_No_WB;
+	assign is_Match				= IssueNoBuff[I_RNo] == I_Issue_No_WB;
 
-	assign Sel_Src				= SelBuff[RNo];
+	assign Sel_Src				= SelBuff[I_RNo];
 	assign O_Data				= I_Data[Sel_Src];
 
-	assign O_Bypass				= is_Match & ~Empty;
+	assign O_Bypass				= is_Match & ~Empty & I_En_WB;
 
 
-	//// Module: Ring-Buffer Controller
-	RingBuffCTRL #(
-		.NUM_ENTRY(			NUM_ENTRY					)
-	) RingBuffCTRL
-	(
-		.clock(				clock						),
-		.reset(				reset						),
-		.I_We(				We							),
-		.I_Re(				Re							),
-		.O_WAddr(			WNo							),
-		.O_RAddr(			RNo							),
-		.O_Full(			Full						),
-		.O_Empty(			Empty						),
-		.O_Num(											)
-	);
+	always_ff @( posege clock ) begin
+		if ( reset ) begin
+			for ( int i=0; i<NUM_ENTRY; ++i ) begin
+				SelBuff[i]		<= 0;
+				IssueNoBuff[i]	<= 0;
+			end
+		end
+		else if ( I_Store ) begin
+			SelBuff[I_WNo]		<= I_Sel;
+			IssueNoBuff[I_WNo]	<= I_Issue_No;
+		end
+	end
 
 endmodule
