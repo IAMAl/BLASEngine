d27834f7baf8aefd2fc92a485f09f94e1893801f
takano <rentsrule@gmail.com>
2024-09-17 11:53:06 +0900

    code refinement

--- src_hdl/utils/RingBuff.sv
+++ src_hdl/utils/RingBuff.sv
@@ -13,7 +13,7 @@ module RingBuff
 #(
 	parameter int NUM_ENTRY		= 16,
 	parameter int WIDTH_ENTRY	= $clog2(NUM_ENTRY),
-	parameter int TYPE			= logic[31:0]
+	parameter typedef TYPE		= logic[31:0]
 )(
 	input						clock,
 	input						reset,
