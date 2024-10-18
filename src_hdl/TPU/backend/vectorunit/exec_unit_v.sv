6dc381f019c73c01b823852a2c89f4c0d4cb12f7
takano <rentsrule@gmail.com>
2024-10-18 13:56:51 +0900

    update: add path writing data into pipe reg in vector lane unit

--- src_hdl/TPU/backend/vectorunit/exec_unit_v.sv
+++ src_hdl/TPU/backend/vectorunit/exec_unit_v.sv
@@ -75,9 +75,43 @@ module ExecUnit_V
 	logic						Re;
 	data_t						Mv_Data;
 
+	logic						RegMoveOp;
+	logic						CommonMov;
+	logic						PMov;
+	logic						PMov0;
+	logic						PMov1;
+
+	data_t						Src_Data2;
+
 	data_t						PData;
 	data_t						Data0;
 	data_t						Data1;
+
+	assign RegMoveOp			= I_Req & ( I_Command.instr.op.OpType == 2'b00 ) & ( I_Command.instr.op.OpClass == 2'b11 );
+	assign CommonMov			= RegMoveOp & ( I_Command.instr.op.OpCode == 2'b01 );
+	assign PMov					= RegMoveOp & ( I_Command.instr.op.OpCode == 2'b10 ) & I_Command.instr.src1.v;
+	assign PMov0				= PMov & ( I_Command.instr.src1.idx == '0 );
+	assign PMov1				= PMov & ( I_Command.instr.src1.idx == '1 );
+
+	assign is_Adder				= I_En & ( I_Token.op.OpClass == 2'b00 );
+	assign is_Mlter				= I_En & ( I_Token.op.OpClass == 2'b01 );
+
+	assign MAU_Token.v			= I_Command.v;
+	assign MAU_Token.op.OPType	= I_Command.op.OPType;
+	assign MAU_Token.op.OPClass	= ( PMov0 ) ?	2'b01 :
+									( PMov1 ) ?	2'b00 :
+												I_Command.op.OPClass;
+	assign MAU_Token.op.OPCode	= ( PMov0 ) ?	2'b00 :
+									( PMov1 ) ?	2'b00 :
+												I_Command.op.OPCode;
+	assign MAU_Token.dst		= I_Command.dst;
+	assign MAU_Token.slice_len	= I_Command.slice_len;
+	assign MAU_Token.issue_no	= I_Command.issue_no;
+	assign MAU_Token.path		= I_Command.path;
+
+	assign Src_Data2			= ( PMov0 ) ?	32'h78000000 :
+									( PMov1 ) ? 32'h00000000 :
+												I_Src_Data2;
 
 
 	assign We					= RegMove & ( ( LifeMAU != '0 ) | ( LifeLdSt != '0 ) );
@@ -86,14 +120,10 @@ module ExecUnit_V
 										( I_Command.instr.op.OpClass == 2'b11 ) &
 										( |I_Command.instr.op.OpCode );
 
-	assign PData				= ( I_Req & ( I_Command.instr.op.OpType == 2'b00 ) &
-									( I_Command.instr.op.OpClass == 2'b11 ) ) ?
-										( I_Command.instr.op.OpCode == 2'b01 ) ?									I_Src_Data1 :
-										( I_Command.instr.op.OpCode == 2'b10 ) ?
-												( I_Command.instr.src1.v & ( I_Command.instr.src1.idx == '0 ) ) ?	Data0 :
-												( I_Command.instr.src1.v & ( I_Command.instr.src1.idx == '1 ) ) ?	Data1 :
-																													'0 :
-																				'0
+	assign PData				= ( CommonMov) ?	I_Src_Data1 :
+									( PMov0 ? )		Data0 :
+									( PMov1 ? )		Data1 :
+													'0 :
 
 	assign MAU_Req				= I_Req & ( I_Command.instr.op.OpType == 2'b00 );
 
@@ -138,11 +168,11 @@ module ExecUnit_V
 		.reset(				reset					),
 		.I_En(				I_En					),
 		.I_Data1(			I_Src_Data1				),
-		.I_Data2(			I_Src_Data2				),
+		.I_Data2(			Src_Data2				),
 		.I_Data3(			I_Src_Data3				),
 		.I_Re_p0(			I_Re_p0					),
 		.I_Re_p1(			I_Re_p1					),
-		.I_Token(			MA_Token				),
+		.I_Token(			MAU_Token				),
 		.O_Valid(			Valid_MAU				),
 		.O_Data(			Data_MAU				),
 		.O_Data0(			Data0					),
